eff0513ae00e29e6e8c3464acc1b27a3d7c6ebc4
Erika Pauwels <erika.pauwels@gmail.com>
2017-06-04 19:52:59 +0200

    Add absent option to form

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,16 +4,27 @@ export default Ember.Controller.extend({
   store: Ember.inject.service(),
   amount: 1,
   amounts: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  isAbsent: null,
+  isPresent: Ember.computed('isAbsent', function() {
+    return this.get('isAbsent') === false;
+  }),
   actions: {
     submit() {
+      const amount = this.get('isPresent') ? this.get('amount') : 0;
       const subscription = this.get('store').createRecord('subscription', {
         name: this.get('name'),
-        amount: this.get('amount'),
+        amount: amount,
         vegetable: this.get('vegetable')
       });
       subscription.save().then(() => {
         this.transitionToRoute('subscribed');
       });
+    },
+    setIsAbsent() {
+      this.set('isAbsent', true);
+    },
+    setIsPresent() {
+      this.set('isAbsent', false);
     }
   }
 });
